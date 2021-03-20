a602520d5e4ac609f9e1f257e12767eba033790d
GITHUB-Yash-Anand <76806913+GITHUB-Yash-Anand@users.noreply.github.com>
2021-03-20 13:50:09 +0530

    Create Game.c

---
+++ Game.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{   int a;
+    printf("Enter the type of system you want to HACK\n");
+    sleep(2);
+    printf("Enter 1 for Windows\n");
+    sleep(2);
+    printf("Enter 2 for Mac\n");
+    scanf("%d", &a );
+    printf("You entered %d for " , a);
+      if (a==1)
+       {
+        printf("WINDOWS");
+       }
+      else
+       {
+        printf("MAC");
+       }
+    printf("\nProcess initialization in 2 sec ");
+    sleep(2);
+    printf("\n1%%");
+    sleep(1);
+    printf("\n5%%");
+    sleep(1);
+    printf("\n8%%");
+    sleep(1);
+    printf("\n17%%");
+    sleep(1);
+    printf("\n24%%");
+    sleep(1);
+    printf("\n29%%");
+    sleep(1);
+    printf("\n45%%");
+    sleep(1);
+    printf("\n56%%");
+    sleep(1);
+    printf("\n68%%");
+    sleep(1);
+    printf("\n77%%");
+    sleep(1);
+    printf("\n84%%");
+    sleep(1);
+    printf("\n99%%");
+
+    printf("\n\nHacking Completed\n");
+    sleep(1);
+    printf("\nServer is shut down ");
+    
+    return 0;
+}
